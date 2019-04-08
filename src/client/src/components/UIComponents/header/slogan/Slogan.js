9b951f8dd6a4d72a42106827994c501f224cb3ca
kristinagerdt <gerdt.kristina@gmail.com>
2019-04-08 16:35:12 +0200

    add styled-components

--- src/client/src/components/UIComponents/header/slogan/Slogan.js
+++ src/client/src/components/UIComponents/header/slogan/Slogan.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    position: relative;
+    margin-top: 4%;
+`;
+
+const Name = styled.p`
+    font-family: Sans-Serif;
+    font-size: 25px;
+    fontStyle: italic;
+    text-align: right;
+    color: #5f8420;
+`;
 
 class Slogan extends Component {
     render() {
         return (
-            <div style={sloganPosition}>
-                <p style={projectSlogan}>The best way to keep your budget on track</p>
-            </div>
+            <Wrapper>
+                <Name>The best way to keep your budget on track</Name>
+            </Wrapper>
         )
     }
 }
 
 export default Slogan;
-
-const projectSlogan = {
-    fontFamily: "Sans-Serif",
-    fontSize: "25px",
-    fontStyle: "italic",
-    textAlign: "right",
-    color: "#5f8420"
-};
-
-const sloganPosition = {
-    position: "relative",
-    marginTop: "4%"
-};
--- src/client/src/components/UIComponents/header/title/Title.js
+++ src/client/src/components/UIComponents/header/title/Title.js
@@ -1,24 +1,50 @@
 import React, {Component} from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    position: relative;
+`;
+
+const Name = styled.p`
+    font-family: Sans-Serif;
+    font-size: 50px;
+    text-align: left;
+    color: #5f8420;
+`;
 
 class Title extends Component {
     render() {
         return (
-            <div style={titlePosition}>
-                <p style={projectTitle}>Budget</p>
-            </div>
+            <Wrapper>
+                <Name>Budget</Name>
+            </Wrapper>
         )
     }
 }
 
 export default Title;
 
-const projectTitle = {
-    fontFamily: "Sans-Serif",
-    fontSize: "50px",
-    textAlign: "left",
-    color: "#5f8420"
-};
-
-const titlePosition = {
-    position: "relative",
-};
+// const Title = ({className}) => {
+//     return (
+//         <div className={className}>
+//             <div className={"title__position"}>
+//                 <p className={"title__name"}>Budget</p>
+//             </div>
+//         </div>
+//     )
+// };
+//
+// const StyledTitle = styled(Title)`
+//     .title__position{
+//         position: relative;
+//     }
+//
+//     .title__name{
+//         font-family: Sans-Serif;
+//         font-size: 50px;
+//         text-align: left;
+//         color: #5f8420;
+//     }
+// `;
+//
+// export default StyledTitle;
