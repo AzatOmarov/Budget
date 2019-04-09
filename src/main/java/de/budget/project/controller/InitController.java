89ceb42159bdf900c8c5938c20cfc2f6a78df745
t-r-student <kst369@mail.ru>
2019-04-09 18:02:39 +0200

    init Controller

--- src/main/java/de/budget/project/controller/InitController.java
+++ src/main/java/de/budget/project/controller/InitController.java
@@ -2,11 +2,13 @@ package de.budget.project.controller;
 
 import de.budget.project.model.types.CategoryType;
 import de.budget.project.model.types.CurrencyType;
+import de.budget.project.model.web.CategoryTypeWebDto;
 import de.budget.project.model.web.CategoryWebDto;
 import de.budget.project.model.web.CurrencyWebDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,30 +17,13 @@ import java.util.List;
 @RequestMapping("/api")
 public class InitController {
 
-    private final List<CategoryType> categoryList = Arrays.asList(CategoryType.values());
-    private final List<CurrencyType> currencyTypes = Arrays.asList(CurrencyType.class.getEnumConstants());
+    private CategoryWebDto categoryWebDto;
+    private CurrencyWebDto currencyWebDto;
+    private CategoryTypeWebDto categoryTypeWebDto;
 
-    @GetMapping("/initdata/categories")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public List<CategoryWebDto> getCategoryTypeList() {
-        List<CategoryWebDto> categoryWebDtos = new LinkedList<>();
-        for (CategoryType category: categoryList) {
-            CategoryWebDto categoryWebDto = new CategoryWebDto(category.getName(),category.getId());
-            categoryWebDtos.add(categoryWebDto);
-        }
-        return categoryWebDtos;
+    @PostConstruct
+    private void init(){
+
     }
 
-    @GetMapping("/initdata/currencies")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public List<CurrencyWebDto> getCurrencyTypeList() {
-        List<CurrencyWebDto> currencyWebDtos = new LinkedList<>();
-        for (CurrencyType currency: currencyTypes) {
-            CurrencyWebDto currencyWebDto = new CurrencyWebDto(currency.getId().longValue(), currency.getName(), currency.getDescription());
-            currencyWebDtos.add(currencyWebDto);
-        }
-        return currencyWebDtos;
-    }
 }
